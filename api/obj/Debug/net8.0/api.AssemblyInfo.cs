8834b42ac2660a622ad5e0f949251958d9f31113
loregbrw <loregobara@gmail.com>
2024-11-08 13:33:11 -0300

    fix #17: remove obj

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abb0275df57b332d38c0784f8c7a402120de7c74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ab5db7b5c577166899afbf545cf2acc37712e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
