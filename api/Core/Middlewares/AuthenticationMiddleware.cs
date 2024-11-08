a22e920fe8b901459ed411fda88a2962eb2a3741
yasmi <yas1cf1@gmail.com>
2024-11-08 13:31:52 -0300

    add: authentication middleware

---
+++ api/Core/Middlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,41 @@
+
+namespace Core.Middlewares;
+
+public class AuthenticationMiddleware : IMiddleware
+{
+    // private readonly JwtService _jwtService;
+    private readonly string[] _pathsToSkip;
+
+    // public AuthenticationMiddleware(JwtService jwtService)
+    // {
+    //     _jwtService = jwtService;
+    //     _pathsToSkip = new []
+    //     {
+    //         "/api/v1/login",
+    //         "/api/v1/users/register",
+    //     };
+    // }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        bool mustSkip = _pathsToSkip.Contains(
+                    context.Request.Path.Value,
+                    StringComparer.OrdinalIgnoreCase);
+
+        if (mustSkip)
+        {
+            await next.Invoke(context);
+            return;
+        }
+
+        // var auth = context.Request.Headers.Authorization.FirstOrDefault()
+        //         ?? throw new InvalidHeadersException("Missing authorization header.");
+        
+        // var token = auth.Split(" ")[1]
+        //         ?? throw new InvalidHeadersException("Authorization header should be a bearer token.");
+
+        // _jwtService.ValidateToken(token);   
+
+        await next.Invoke(context);
+    }
+}
