1bb9310a57f64898f7867d25855957355efb3a26
MuryloSaladino <murylosaladino@gmail.com>
2024-11-12 16:55:42 -0300

    feat: button styles

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,15 @@
 import Button from "./components/Button";
 
+import "./styles/reset.css"
+import "./styles/global.css"
+
 export default function App() {
     return (
         <>
-            <Button variant="outlined">Bosch</Button>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "70vh", gap: "10px" }}>
+                <Button variant="outlined">Bosch</Button>
+                <Button variant="contained">Bosch</Button>
+            </div>
         </>
     )
 }
--- ui/src/components/Button/index.tsx
+++ ui/src/components/Button/index.tsx
@@ -1,6 +1,14 @@
-import { Button } from "@mui/material";
-import { styled } from "@mui/system";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+import styles from "./styles.module.css"
 
-export default styled(Button)(() => ({
-    borderRadius: 0,
-}))
+interface IButtonProps extends ComponentPropsWithoutRef<'button'> {
+    variant?: "contained" | "outlined";
+}
+
+export default forwardRef<HTMLButtonElement, IButtonProps>(({ variant = "outlined", ...props}, ref) => 
+    <button
+        ref={ref}
+        {...props}
+        className={`${styles.common} ${styles[variant]}`}
+    />
+)
