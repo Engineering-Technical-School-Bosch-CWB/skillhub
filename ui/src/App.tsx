33bfda3bf2b34606dcb20af86e79a42c2732d971
MuryloSaladino <murylosaladino@gmail.com>
2024-11-12 15:39:51 -0300

    feat: basic button

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,9 @@
+import Button from "./components/Button";
+
 export default function App() {
     return (
         <>
-          
+            <Button variant="outlined">Bosch</Button>
         </>
     )
 }
---
+++ ui/src/components/Button/index.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@mui/material";
+import { styled } from "@mui/system";
+
+export default styled(Button)(() => ({
+    borderRadius: 0,
+}))
