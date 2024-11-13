76e8ea472c17efaefe6b1b843aae5600cd3c82f9
MuryloSaladino <murylosaladino@gmail.com>
2024-11-13 16:34:21 -0300

    fix: app config

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,23 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { UserProvider } from "./contexts/userContext";
 import "./styles/global.css"
-import Form, { IField } from "./components/Form";
+import router from "./routes/routes";
 
 export default function App() {
-
-    const fields:IField[] = [
-        { name: "email", label: "Email" },
-        { name: "password", label: "Password", type: "password" },
-    ]
-
     return (
-        <>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "70vh", gap: "30px", padding: "100px" }}>
-                <div style={{ maxWidth: "500px", width: "100%" }}>
-                    <Form 
-                        onSubmit={(payload) => console.log(payload)}
-                        fields={fields}
-                    />
-                </div>
-            </div>
-        </>
+        <UserProvider>
+            <RouterProvider router={router}/>
+        </UserProvider>
     )
 }
--- ui/src/components/Form/index.tsx
+++ ui/src/components/Form/index.tsx
@@ -16,6 +16,40 @@ interface IFormProps<T> {
     submitText?: string;
 }
 
+/**
+ * Reusable `Form` component for rendering a form with customizable fields.
+ * Uses `react-hook-form` for form management, making it easy to add fields dynamically,
+ * handle validation, and process submissions.
+ *
+ * @template T - The type of form field values, extending FieldValues to support react-hook-form integration.
+ * 
+ * @param {IFormProps<T>} props - The properties object for configuring the `Form` component.
+ * @param {(payload: T) => Promise<void> | void} props.onSubmit - Function to handle form submission, receiving form data as an argument.
+ * @param {string} [props.customClassName] - Optional CSS class name for styling the form container.
+ * @param {IField<T>[]} props.fields - Array of field configurations, each specifying a name, label, and optional type (e.g., "text" or "password").
+ * @param {string} [props.submitText="Submit"] - Optional text for the submit button; defaults to "Submit" if not provided.
+ *
+ * @returns {JSX.Element} The form element, with fields mapped to `Input` components based on `fields` configuration.
+ *
+ * @example
+ * ```tsx
+ * const fields: IField[] = [
+ *   { name: "email", label: "Email", type: "text" },
+ *   { name: "password", label: "Password", type: "password" }
+ * ];
+ *
+ * function handleFormSubmit(data) {
+ *   console.log(data);
+ * }
+ *
+ * <Form
+ *   onSubmit={handleFormSubmit}
+ *   fields={fields}
+ *   customClassName="custom-form"
+ *   submitText="Sign In"
+ * />
+ * ```
+ */
 export default function Form<T extends FieldValues>({
     onSubmit,
     customClassName,
