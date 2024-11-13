0502ca9278f5c1d3649d4f81b3f46092359da409
AndreLuisPLuz <andreluispluz@gmail.com>
2024-11-13 16:40:16 -0300

    feat #31: implemented fetches on position service.

--- api/Domain/Entities/position/Models/PositionOutbound.cs
+++ api/Domain/Entities/position/Models/PositionOutbound.cs
@@ -1,10 +1,19 @@
 using api.Domain.Services.Pagination;
+using Api.Domain.Models;
 
 namespace api.Domain.Models
 {
     public class PositionResponse
     {
         public required string Name { get; init; }
+
+        public static PositionResponse ToResponse(Position position)
+        {
+            return new PositionResponse()
+            {
+                Name = position.Name,
+            };
+        }
     }
 
     public class PaginatedPositionsResponse
