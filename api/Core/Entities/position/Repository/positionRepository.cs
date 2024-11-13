03dc1b547607b87ff9f07520d6adc60736e72302
AndreLuisPLuz <andreluispluz@gmail.com>
2024-11-13 16:16:28 -0300

    feat #31: implemented paginated fetches on the position repository.

--- api/Core/Entities/position/Repository/positionRepository.cs
+++ api/Core/Entities/position/Repository/positionRepository.cs
@@ -1,11 +1,30 @@
 using Genesis.Core.Repositories;
 using Api.Domain.Repositories;
 using Api.Domain.Models;
+using api.Domain.Services.Pagination;
+using api.Core.Services.Pagination;
 
 namespace Api.Core.Repositories;
 
-public class PositionRepository(Project_eContext context) 
-    : BaseRepository<Position>(context), IPositionRepository
+public class PositionRepository(
+        Project_eContext context,
+        PaginationService paginationService)
+    :
+        BaseRepository<Position>(context),
+        IPositionRepository
 {
+    private readonly PaginationService _paginationService = paginationService;
 
+    public (IEnumerable<Position>, PaginationInfo) GetPaginated(PaginationOptions options)
+    {
+        var result = _paginationService.Paginate(GetAllNoTracking(), options);
+        return result;
+    }
+
+    public async Task<(IEnumerable<Position>, PaginationInfo)> GetPaginatedAsync(PaginationOptions options)
+    {
+        var result = await _paginationService.PaginateAsync(
+            GetAllNoTracking(), options);
+        return result;
+    }
 }
