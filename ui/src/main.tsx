57bc39666148fe48bfbfc841df1187d4d9ffa560
nycollassobolevski <sobolevski.academy@gmail.com>
2024-11-13 16:40:42 -0300

    feat #70: i18n implemented

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,6 +4,8 @@ import { UserProvider } from './contexts/userContext.tsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/routes.ts'
 
+import './hooks/i18n/i18n';
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <UserProvider>
