8fbfe0c53d7164bc36de326fa8feb1eef8791d74
yasmi <yas1cf1@gmail.com>
2024-11-13 15:35:25 -0300

    #72 chore: cleaned up some bits

--- ui/src/routes/routes.ts
+++ ui/src/routes/routes.ts
@@ -1,11 +1,11 @@
-import { createBrowserRouter, RouteObject } from "react-router-dom";
-import routes from "../constants/routes";
+import { createBrowserRouter, RouteObject } from "react-router-dom"
+import routes from "../constants/routes"
 
 const routesDom:RouteObject[] = Object.values(routes).map(route => ({
     path: route.PATH,
     element: route.ELEMENT
 }))
 
-const router = createBrowserRouter(routesDom);
+const router = createBrowserRouter(routesDom)
 
-export default router;
+export default router
