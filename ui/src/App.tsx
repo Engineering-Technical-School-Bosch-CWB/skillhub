cc262f9b81f00bcda917a932cb99ccab14ca9a55
MuryloSaladino <murylosaladino@gmail.com>
2024-11-13 16:59:49 -0300

    checkpoint

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from "react-router-dom";
 import { UserProvider } from "./contexts/userContext";
 import "./styles/global.css"
 import router from "./routes/routes";
+import Header from "./components/Header";
 
 export default function App() {
     return (
         <UserProvider>
-            <RouterProvider router={router}/>
+            {/* <RouterProvider router={router}/> */}
+            <Header/>
         </UserProvider>
     )
 }
