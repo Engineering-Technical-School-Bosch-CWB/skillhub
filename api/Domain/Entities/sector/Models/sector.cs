dde54de224d4663fd418489c5c1da5d8d06ba57c
AndreLuisPLuz <andreluispluz@gmail.com>
2024-11-13 14:26:48 -0300

    refact #74: name initializing with empty string on sector entity.

--- api/Domain/Entities/sector/Models/sector.cs
+++ api/Domain/Entities/sector/Models/sector.cs
@@ -4,7 +4,7 @@ namespace Api.Domain.Models;
 
 public partial class Sector : IEntity
 {
-  public string Name { get; set; }
+  public string Name { get; set; } = string.Empty;
   public bool IsActive { get; set; }
 
   public IEnumerable<User> Users { get; set; } = [];
