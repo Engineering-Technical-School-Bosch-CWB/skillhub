4f43393aa172aca6fb26812869e57f515f2c4335
MuryloSaladino <murylosaladino@gmail.com>
2024-11-14 13:26:09 -0300

    fix: doc for form

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,15 @@
-import { RouterProvider } from "react-router-dom";
 import { UserProvider } from "./contexts/userContext";
 import "./styles/global.css"
+
+import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
+
 import Header from "./components/Header";
 
 export default function App() {
     return (
         <UserProvider>
-            {/* <RouterProvider router={router}/> */}
+            <RouterProvider router={router}/>
             <Header/>
         </UserProvider>
     )
