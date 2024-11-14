4bfc89e2915c4580eef851aecf4ef34304e33ed2
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-11-14 16:24:44 -0300

    feat #33: Course service implemented, IsActive column added to the Course entity, and small changes in the user service implementation.

---
+++ api/Controllers/Course/CourseController.cs
@@ -0,0 +1,62 @@
+using api.Domain.Services.Pagination;
+using Api.Core.Services;
+using Api.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/course")]
+    public class CourseController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> RegisterCourse(
+            [FromServices] CourseService CourseService,
+            [FromBody] CourseCreatePayload payload)
+        {
+            var result = await CourseService.CreateCourse(payload);
+            return Created("/api/v1/Courses/register", result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult> GetCourseById(
+            [FromServices] CourseService CourseService,
+            int id)
+        {
+            var result = await CourseService.GetCourseById(id);
+            return new OkObjectResult(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllCourse(
+            [FromServices] CourseService CourseService,
+            [FromBody] PaginationOptions pagination)
+        {
+            var result = await CourseService.GetCourses(pagination);
+            return new OkObjectResult(result);
+        }
+
+        [HttpPatch]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateCourse(
+            [FromServices] CourseService service,
+            [FromBody] CourseUpdatePayload payload,
+            int id)
+        {
+            var result = await service.UpdateCourse(id, payload);
+            return new OkObjectResult(result);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteCourse(
+            [FromServices] CourseService service,
+            int id)
+        {
+            var result = await service.DeleteCourse(id);
+            return new OkObjectResult(result);
+        }
+
+    }
+}
