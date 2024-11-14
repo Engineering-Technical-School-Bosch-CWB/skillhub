096be7f082aa8a74bcd48c3073de63290dd0acb2
AndreLuisPLuz <andreluispluz@gmail.com>
2024-11-14 14:03:39 -0300

    feat #31: configured some services, some still lacking.

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,22 @@
 using api.Core.Middlewares;
+using api.Core.Services.Pagination;
+using Api.Core.JWTService;
+using Api.Core.Repositories;
+using Api.Core.Services;
+using Api.Domain.Repositories;
+using Api.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ErrorHandlingMiddleware>();
+builder.Services.AddSingleton<AuthenticationMiddleware>();
+
+builder.Services.AddSingleton<JwtService>();
+builder.Services.AddSingleton<PaginationService>();
+
+builder.Services.AddScoped<IPositionRepository, PositionRepository>();
+
+builder.Services.AddScoped<IPositionService, PositionService>();
 
 builder.Services.AddExceptionHandler<ErrorHandlingMiddleware>();
 builder.Services.AddProblemDetails();
@@ -20,7 +36,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 
 app.UseHttpsRedirection();
 
