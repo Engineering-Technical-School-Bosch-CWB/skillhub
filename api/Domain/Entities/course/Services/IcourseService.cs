2733cfe6d815e61e14b628e1b2e5d590866ca94a
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-11-18 14:16:42 -0300

    fix #33: responses of services

--- api/Domain/Entities/course/Services/IcourseService.cs
+++ api/Domain/Entities/course/Services/IcourseService.cs
@@ -6,9 +6,9 @@ namespace Api.Domain.Services;
 
 public interface ICourseService : IService<Course>
 {
-    public Task<CourseCreatedOutbound> CreateCourse(CourseCreatePayload payload);
-    public Task<CourseDeletedOutbound> DeleteCourse(int id);
-    public Task<AllCoursesOutbound> GetCourses(PaginationOptions options);
-    public Task<OneCourseOutbound> GetCourseById(int id);
-    public Task<CourseUpdatedOutbound> UpdateCourse(int id, CourseUpdatePayload payload);
+    public Task<CourseResponse> CreateCourse(CourseCreatePayload payload);
+    public Task DeleteCourse(int id);
+    public Task<CoursePaginationResponse> GetCourses(PaginationOptions options);
+    public Task<CourseResponse> GetCourseById(int id);
+    public Task<CourseResponse> UpdateCourse(int id, CourseUpdatePayload payload);
 }
