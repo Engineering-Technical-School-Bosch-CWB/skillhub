dd482b49bf796c35c33929ba4b96c22029341701
MuryloSaladino <murylosaladino@gmail.com>
2024-11-18 13:41:10 -0300

    styles: form background

--- ui/src/pages/Home/index.tsx
+++ ui/src/pages/Home/index.tsx
@@ -1,9 +1,23 @@
+import Form, { IField } from "../../components/Form"
 import Header from "../../components/Header"
 
 export default () => {
+
+    const fields: IField[] = [
+        { name: "email", label: "Email", type: "text" },
+        { name: "password", label: "Password", type: "password" }
+    ];
+
     return (
         <>
             <Header/>
+
+            <div style={{ margin: "20vh auto", width: "500px" }}>
+                <Form
+                    fields={fields}
+                    onSubmit={(data) => console.log(data)}
+                />
+            </div>
         </>
     )
 }
