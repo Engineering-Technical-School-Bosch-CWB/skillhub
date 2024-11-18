3f595f7ffced015670837b71728a8cdce3984fea
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-11-18 15:10:46 -0300

    refactor #33: some little alterations on course

--- api/Domain/Entities/course/Models/CourseDTO.cs
+++ api/Domain/Entities/course/Models/CourseDTO.cs
@@ -3,11 +3,10 @@ namespace Api.Domain.Models
     public record CourseDTO(
         string? Name,
         string? Abbreviation,
-        int? OccupationAreaId,
         OccupationAreaDTO? OccupationArea
     ){
         public static CourseDTO Map(Course course)
-            => new(course.Name, course.Abbreviation, course.DefaultOccupationArea.Id, OccupationAreaDTO.Map(course.DefaultOccupationArea));
+            => new(course.Name, course.Abbreviation, OccupationAreaDTO.Map(course.DefaultOccupationArea));
     };
 
 }
--- api/Domain/Entities/course/Models/CourseResponses.cs
+++ api/Domain/Entities/course/Models/CourseResponses.cs
@@ -4,15 +4,15 @@ namespace Api.Domain.Models;
 
 public class CourseResponse
 {
-    public required CourseDTO Course { get; set; }
     public required string Message { get; set; }
+    public required CourseDTO Data { get; set; }
 
     public static CourseResponse Map(Course course, string text)
     {
         return new CourseResponse()
         {
-            Course = CourseDTO.Map(course),
-            Message = text
+            Message = text,
+            Data = CourseDTO.Map(course)
         };
     }
 };
@@ -32,42 +32,3 @@ public class CoursePaginationResponse
         };
     }
 }
-
-    // public class CourseCreatedResponse
-    // {
-    //     public int CourseId { get; set; }
-    //     public required string Name { get; set; }
-    //     public string? Abbreviation { get; set; }
-    //     public required string OccupationAreaId { get; set; }
-
-    //     public static CourseCreatedResponse Map(Course course, OccupationArea area)
-    //     {
-    //         return new CourseCreatedResponse()
-    //         {
-    //             CourseId = course.Id,
-    //             OccupationArea = area.Name,
-    //             Name = course.Name,
-    //             Abbreviation = course.Abbreviation
-    //         };
-    //     }
-    // };
-
-
-    // public class CourseUpdatedOutbound
-    // {
-    //     public string? Name { get; set; }
-    //     public string? Abbreviation { get; set; }
-    //     public int? OccupationAreaId { get; set; }
-
-    //     public static CourseUpdatedOutbound Map(Course course){
-    //         return new CourseUpdatedOutbound()
-    //         {
-    //             Name = course.Name,
-    //             Abbreviation = course.Abbreviation,
-    //             OccupationAreaId = course.DefaultOccupationArea.Id,
-    //         };
-    //     }
-    // }
-
-
-
