00ce2524eef1cc672f1d72135b4d9e45a3218633
MuryloSaladino <murylosaladino@gmail.com>
2024-11-19 13:59:35 -0300

    feat: route map

---
+++ ui/src/router/map.ts
@@ -0,0 +1,9 @@
+export enum RouteMap {
+    LOGIN = "/login",
+
+    HOME = "/",
+    BIRTHDAYS = "/birthdays",
+
+    SCHOOL_CONTENT = "/school-content",
+    CURRICULAR_UNITY_BY_ID = "/school-content/curricular-unitys/"
+}
