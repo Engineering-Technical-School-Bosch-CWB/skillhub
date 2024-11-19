d28a031fc3554df84ba41cc59c5c2a8dbed619d2
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2024-11-19 14:01:31 -0300

    feat: add Sector services, payloads, and responses; update Course payload to class with body validation.

--- api/Domain/Entities/sector/Models/sector.cs
+++ api/Domain/Entities/sector/Models/sector.cs
@@ -6,6 +6,5 @@ public partial class Sector : IEntity
 {
   public string Name { get; set; } = string.Empty;
   public bool IsActive { get; set; }
-
   public ICollection<User> Users { get; set; } = [];
 }
--- api/Domain/Entities/sector/Services/IsectorService.cs
+++ api/Domain/Entities/sector/Services/IsectorService.cs
@@ -1,3 +1,4 @@
+using Api.Domain.Services.Pagination;
 using Genesis.Domain.Services;
 using Api.Domain.Models;
 
@@ -5,5 +6,9 @@ namespace Api.Domain.Services;
 
 public interface ISectorService : IService<Sector>
 {
-
+    public Task<SectorResponse> CreateSector(SectorCreatePayload payload);
+    public Task DeleteSector(int id);
+    public Task<SectorPaginationResponse> GetSectors(PaginationOptions options);
+    public Task<SectorResponse> GetSectorById(int id);
+    public Task<SectorResponse> UpdateSector(int id, SectorUpdatePayload payload);
 }
