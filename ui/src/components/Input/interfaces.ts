3630dab5e29693601bc536cb440e977c435cbec9
MuryloSaladino <murylosaladino@gmail.com>
2024-11-26 17:00:21 -0300

    checkpoint

--- ui/src/components/Input/interfaces.ts
+++ ui/src/components/Input/interfaces.ts
@@ -2,10 +2,11 @@ import { IInputDateProps } from "./InputDate"
 import { IInputTextProps } from "./InputText"
 import { IInputContainerProps } from "./InputContainer"
 import { IInputPasswordProps } from "./InputPassword"
+import { ComponentPropsWithoutRef } from "react"
 
 export type IInputType = "text" | "password" | "date" | "email"
 
-export interface IRootInputProps extends IInputContainerProps {
+export interface IRootInputProps extends IInputContainerProps, ComponentPropsWithoutRef<'input'> {
     id?: string
     type?: IInputType
     error?: boolean
