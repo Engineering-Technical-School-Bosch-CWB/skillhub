6b34074e2a2e057398d17acfff627da27a378a34
nycollassobolevski <sobolevski.academy@gmail.com>
2025-02-06 14:00:38 -0300

    feat create subject area working

--- ui/src/pages/SchoolContent/components/CreateModals/SubjectAreaCreateModal.tsx
+++ ui/src/pages/SchoolContent/components/CreateModals/SubjectAreaCreateModal.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react"
 import { ICreateModalProps } from "./_CreateModal.interface"
-import { ICourse } from "@/interfaces/models/ICourse";
+
+import styles from '../DeleteModals/styles.module.css'
+import Input from "@/components/Input";
+import { SubjectArea } from "@/interfaces/models/ISubjectArea";
 
 export default ({onChange}: ICreateModalProps) => {
 
-    const [data, setData] = useState<ICourse>();
+    const [data, setData] = useState<SubjectArea>();
 
-    const change = (key: keyof ICourse, value: any) => {
+    const change = (key: keyof SubjectArea, value: any) => {
         setData(prev => ({
             ...prev!,
             [key]: value
@@ -18,7 +21,8 @@ export default ({onChange}: ICreateModalProps) => {
     }, [data]);
 
     return (
-        <>
-        </>
+        <section className={styles.content_section}>
+            <Input label="Name" value={data?.name} onChange={(e) => change("name", e.target.value)} /> 
+        </section>
     )
 }
