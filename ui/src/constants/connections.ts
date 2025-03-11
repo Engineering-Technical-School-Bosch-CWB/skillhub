deba7120dbb029d7ad81138b1bd8556dcf8b4c10
trevisharp <leonardotrevisansilio@gmail.com>
2025-03-11 15:16:27 -0300

    fix configuration of internal api

--- ui/src/constants/connections.ts
+++ ui/src/constants/connections.ts
@@ -1,4 +1,4 @@
 export enum EConnections {
-    InternalAPI = "http://localhost:5246/api/v1",
+    InternalAPI = "http://localhost:5246/SkillHub/api/v1",
     ProdApi = "https://ctp-ets.br.bosch.com/SkillHub/api/v1"
 }
