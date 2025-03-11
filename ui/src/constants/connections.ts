83c08719e49dfb64ca9171657bb17166ca7c86b9
nycollassobolevski <sobolevski.academy@gmail.com>
2025-03-11 09:46:06 -0300

    fix url to prod

--- ui/src/constants/connections.ts
+++ ui/src/constants/connections.ts
@@ -1,4 +1,4 @@
 export enum EConnections {
     InternalAPI = "http://localhost:5246/api/v1",
-    ProdApi = "https://ctp-ets.br.bosch.com/api/v1"
+    ProdApi = "https://ctp-ets.br.bosch.com/skillhub/api/v1"
 }
