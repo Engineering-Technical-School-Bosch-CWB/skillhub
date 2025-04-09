2781e5903ca943325b5ff0ea2206cc1d24119d1a
OsvaldoComCH <eduhrsilva@gmail.com>
2025-04-09 11:23:10 -0300

    Feat. #307 - Added new database migration

---
+++ api/Migrations/20250409142126_FixedIsActiveFieldNames.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedIsActiveFieldNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "eventType",
+                newName: "is_active");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "eventMember",
+                newName: "is_active");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "classEvent",
+                newName: "is_active");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_active",
+                table: "eventType",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "is_active",
+                table: "eventMember",
+                newName: "IsActive");
+
+            migrationBuilder.RenameColumn(
+                name: "is_active",
+                table: "classEvent",
+                newName: "IsActive");
+        }
+    }
+}
